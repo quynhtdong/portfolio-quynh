f05e67bd75a9fbec4e2062fa0ce03616ff2c2a8d
Quynh <quynhdongikn@gmail.com>
2022-01-13 13:21:16 -0600

    add projects

--- notus-nextjs-main/pages/index.js
+++ notus-nextjs-main/pages/index.js
@@ -65,7 +65,6 @@ export default function Index() {
                   <h5 className="text-xl font-semibold pb-4 text-center">
                     PTE Magic
                   </h5>
-                  <Link href="/auth/login">
                     <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                       <img
                         alt="..."
@@ -73,37 +72,32 @@ export default function Index() {
                         src="/img/pte.png"
                       />
                     </div>
-                  </Link>
                 </div>
 
                 <div className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
-                    BIDMA
+                    Space-stagram
                   </h5>
-                  <Link href="/profile">
                     <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                       <img
                         alt="..."
                         className="align-middle border-none max-w-full h-auto rounded-lg"
-                        src="/img/profile.jpg"
+                        src="/img/space.png"
                       />
                     </div>
-                  </Link>
                 </div>
 
                 <div className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
-                    Nauvus
+                    Blog with Authentication
                   </h5>
-                  <Link href="/landing">
                     <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                       <img
                         alt="..."
                         className="align-middle border-none max-w-full h-auto rounded-lg"
-                        src="/img/landing.jpg"
+                        src="/img/blog.png"
                       />
                     </div>
-                  </Link>
                 </div>
               </div>
             </div>
@@ -203,6 +197,165 @@ export default function Index() {
                       <h6 className="text-xl mb-1 font-semibold">Pages</h6>
                       <p className="mb-4 text-blueGray-500">
                         <a href="https://ptemagicpractice.com/">PTE Magic</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto" style={{marginBottom: 100, marginTop: 100}}>
+          <div className="flex flex-wrap items-center">
+          <div className="w-full md:w-6/12 px-4 md:px-4 ml-auto">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Project
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                      A webpage that can pull images from NASA’s free APIs, and allow the user to “like” and “unlike” their favourite images. 
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Using
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Java Script, React, Next JS, Material UI, HTML, CSS
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Pages</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        <a href="https://dw6j7.sse.codesandbox.io/">Space-stagram</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 mr-auto">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+                <img
+                  alt="..."
+                  src="/img/space.png"
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                    <polygon
+                      points="-30,95 583,95 583,65"
+                      className="text-blueGray-700 fill-current"
+                    ></polygon>
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    Space-stagram
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    Display ranges of astronomy picture from NASA API that users can like/unlike
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+        <div className="container mx-auto" style={{marginBottom: 100, marginTop: 100}}>
+          <div className="flex flex-wrap items-center">
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blueGray-700">
+                <img
+                  alt="..."
+                  src="/img/blog.png"
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                    <polygon
+                      points="-30,95 583,95 583,65"
+                      className="text-blueGray-700 fill-current"
+                    ></polygon>
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    Blog with Authentication
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    A blog that admin can post/update/delete posts and assign which posts are accessible for each user
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 px-4">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Project
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Building blog from scratches for CRUD application
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Using
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Java Script, React, HTML, CSS, RESTful API, NodeJs, Mongoose, Bcrypt
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Pages</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        <a href="https://github.com/quynhtdong/blog-with-authentication">Blog</a>
                       </p>
                     </div>
                   </div>
